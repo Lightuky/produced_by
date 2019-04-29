948400bff4737e36ebd190a774330415b2a8f960
Yann Le Boudec <47103897+Lightuky@users.noreply.github.com>
2019-04-29 09:33:02 +0200

    Add files via upload

---
+++ language/fr_FR/plugin.lang.php
@@ -0,0 +1,14 @@
+<?php
+$lang['Copyrights'] = 'Droits d\'auteur';
+$lang['Create copyright'] = 'Créer un droit d\'auteur';
+$lang['Edit copyright'] = 'Modifier un droit d\'auteur';
+$lang['Copyright'] = 'Droit d\'auteur';
+$lang['Update copyright'] = 'mettre à jour un droit d\'auteur';
+$lang['Delete copyright'] = 'supprimer un droit d\'auteur';
+$lang['Visible'] = 'Visible';
+$lang['This copyright already exists.'] = 'Ce droit d\'auteur existe déjà';
+$lang['Set copyright'] = 'Régler un droit d\'auteur';
+$lang['With copyright'] = 'Avec un droit d\'auteur';
+$lang['Without copyright'] = 'Sans droit d\'auteur';
+
+?>
